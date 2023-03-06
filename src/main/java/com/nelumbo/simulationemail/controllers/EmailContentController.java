99bf01257fcb3a23a77585cfe83854e9f7502981
leonardolopez1609 <leonardojoselc@ufps.edu.co>
2023-03-06 17:48:09 -0500

    V1.0 Correction user push

--- src/main/java/com/nelumbo/simulationemail/controllers/EmailContentController.java
+++ src/main/java/com/nelumbo/simulationemail/controllers/EmailContentController.java
@@ -8,5 +8,5 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmailContentController {
 
 	
-	//IMPLEMENTAR METODO QUE ENVIE EL EMAIL 
+	//IMPLEMENTAR METODO QUE ENVIE EL EMAIL   
 }
