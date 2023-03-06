4c49d83517d1350cf5d9901b266da09642f51062
leonardolopez1609 <leonardojoselc@ufps.edu.co>
2023-03-06 17:57:36 -0500

    V1.1- Endpoint sendEmail

--- src/main/java/com/nelumbo/simulationemail/controllers/EmailContentController.java
+++ src/main/java/com/nelumbo/simulationemail/controllers/EmailContentController.java
@@ -1,12 +1,29 @@
 package com.nelumbo.simulationemail.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.nelumbo.simulationemail.entities.EmailContent;
+import com.nelumbo.simulationemail.services.IEmailContentService;
 
 @RestController
 @RequestMapping
 public class EmailContentController {
 
+	@Autowired
+	IEmailContentService emailContentService;
 	
 	//IMPLEMENTAR METODO QUE ENVIE EL EMAIL   
+	
+	@PostMapping()
+	public String sendEmail(@RequestBody EmailContent emailContent) {
+		
+		//retornar un JSON
+		
+		return emailContentService.sendEmail(emailContent);
+		
+	}
 }
